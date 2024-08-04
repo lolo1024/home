1c50e308c0e0e264b50103b19c4a862081079b62
lolo1024 <yinlolo1996@gmail.com>
2024-08-04 11:33:07 +0800

    Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Star, Cloud, Fish, Book, Bookmark, Fire, BookmarkRegular, SimCard, UserLock, Qq, Gift } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Star, Cloud, Fish, Book, Bookmark, Fire, BookmarkRegular, SimCard, UserLock, Qq, Gift, Brain } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -74,6 +74,7 @@ const siteIcon = {
   UserLock,
   Qq,
   Gift,
+  Brain,
 };
 
 // 链接跳转
